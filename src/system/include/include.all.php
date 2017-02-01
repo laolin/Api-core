3593c30ff9563b22c3c2ff20b57c4a0f74f8c86e
LaoLin <hi@laolin.com>
2017-02-01 21:38:30 +0800

    内置api: /user/xxx

--- src/system/include/include.all.php
+++ src/system/include/include.all.php
@@ -1,1 +1,10 @@
-<?phprequire api_g('path-sys').'/lib/medoo/medoo.php';require api_g('path-sys').'/include/functions.php';require api_g('path-sys').'/include/class.laolindb.php';require api_g('path-sys').'/include/class.token.bucket.php';require api_g('path-sys').'/include/class.user.php';
+<?php
+
+require_once api_g('path-sys').'/lib/medoo/medoo.php';
+
+require_once api_g('path-sys').'/include/functions.php';
+
+require_once api_g('path-sys').'/include/class.laolindb.php';
+
+require_once api_g('path-sys').'/include/class.token.bucket.php';
+require_once api_g('path-sys').'/include/class.user.php';
