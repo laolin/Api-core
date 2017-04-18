033a55e9de94ca316de891346c4a85d99201680e
LaoLin <hi@laolin.com>
2017-04-18 19:06:07 +0800

    还原

---
+++ src/system/include/include.all.php
@@ -0,0 +1,11 @@
+<?php
+require_once api_g('path-sys').'/include/G.php';// Good tools
+
+require_once api_g('path-sys').'/lib/medoo/medoo.php';
+
+require_once api_g('path-sys').'/include/functions.php';
+
+require_once api_g('path-sys').'/include/class.laolindb.php';
+
+require_once api_g('path-sys').'/include/class.token.bucket.php';
+require_once api_g('path-sys').'/include/class.user.php';
