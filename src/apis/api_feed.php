c7ad0c75ee7e1b172cad1c153e62e9741fa6301f
LaoLin <hi@laolin.com>
2017-05-20 14:22:55 +0800

    /feed/ 系列API

---
+++ src/apis/api_feed.php
@@ -0,0 +1,193 @@
+<?php
+// ================================
+/**
+ *  errcode:
+ *  
+ *  以下编号和客户端代码相关
+ *  202001 error userVerify
+ *  202002 发布内容无效
+ *  202003 获取内容结果为空
+ *  
+ *  以下编号目前和客户端代码不相关
+ *  2021xx feed get err
+ *  202201 undelete err
+ *  
+ */
+
+
+class class_feed {
+  public static function main( $para1,$para2) {
+    $res=API::data(['time'=>time().' - feed is ready.']);
+    return $res;
+  }
+  static function userVerify() {
+    return USER::userVerify();
+  }
+  
+  //test
+  public static function test( ) {
+    $r=self::userVerify();
+    if(!$r)
+      return API::msg(202001,'error userVerify');
+    return API::data('Test passed.');
+  }
+  public static function config( ) {
+    return API::data(EBCONFIG::config());
+  }  
+  
+
+  /**
+   *  API:
+   *    /feed/draft_create
+   *  输入: 
+   *    uid
+   *  
+   *  返回:
+   *    各字段
+   *  
+   *  注：为简化系统， 默认规定只能有一个 草稿
+   *  所以当 uid 用户已有草稿时，此API是返回原有的草稿
+   */
+  public static function draft_init( ) {
+    $r=self::userVerify();
+    if(!$r)
+      return API::msg(202001,'error userVerify');
+    $uid=API::INP('uid');    
+    $r=FEED::draft_get_by_uid($uid);
+    if(!$r) {
+      $r=FEED::draft_create($uid);
+    }
+    return API::data($r);
+  }
+
+  
+  /**
+   *  API:
+   *    /feed/draft_update
+   *  输入: 
+   *    uid
+   *    fid
+   *    其他要更新的字段 : FEED::data_all()函数
+   *  
+   *  返回:
+   *    1 or 0 表示有无更新
+   *  
+   */
+  public static function draft_update( ) {
+    $r=self::userVerify();
+    if(!$r)
+      return API::msg(202001,'error userVerify');
+    
+    $uid=API::INP('uid');
+    $fid=API::INP('fid');
+    
+    //要确保fid是对应一个存在的数据
+    $r=FEED::feed_get($uid,$fid,'draft');
+    if(API::is_error($r)){
+      return $r;
+    }
+
+    $data=FEED::data_all();
+    $r=FEED::feed_update($fid,$data);
+    return API::data($r);
+  }
+
+  /**
+   *  API:
+   *    /feed/draft_publish
+   *  输入: 
+   *    uid
+   *    fid
+   *  
+   *  返回:
+   *    1 or 0 表示有无更新
+   */
+  public static function draft_publish( ) {
+    $r=self::userVerify();
+    if(!$r)
+      return API::msg(202001,'error userVerify');
+
+    $uid=API::INP('uid');    
+    $fid=API::INP('fid');
+    //要确保fid是对应一个存在的数据
+    $r=FEED::feed_get($uid,$fid,'draft');
+    if(API::is_error($r)){
+      return $r;
+    }
+    $err=FEED::feed_validate($r['data']);
+    if($err){
+      return API::msg(202002,$err);
+    }
+    $r=FEED::draft_publish($fid );
+    return API::data($r);
+  }
+  
+  //fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff
+  
+  /**
+   *  API:
+   *    /feed/get
+   *  输入: 
+   *    uid
+   *    fid 
+   *  
+   *  返回:
+   *    各字段
+   */
+  public static function get( ) {
+    $r=self::userVerify();
+    if(!$r)
+      return API::msg(202001,'error userVerify');
+    
+    $uid=API::INP('uid');    
+    $fid=API::INP('fid');
+    $r=FEED::feed_get($uid,$fid,'publish');
+    return $r;
+  }
+ 
+  /**
+   *  API:
+   *    /feed/li
+   *  输入: 
+   *    uid
+   *  
+   *  返回:
+   *    各字段
+   */
+  public static function li( ) {
+    $r=self::userVerify();
+    if(!$r)
+      return API::msg(202001,'error userVerify');
+    
+    $uid=API::INP('uid');    
+    $r=FEED::feed_list($uid,'publish');
+    return $r;
+  }
+  /**
+   *  API:
+   *    /feed/del
+   *  输入: 
+   *    uid
+   *    fid
+   *  
+   *  返回:
+   *    1 or 0 表示有无更新
+   */
+  public static function del( ) {
+    $r=self::userVerify();
+    if(!$r)
+      return API::msg(202001,'error userVerify');
+    
+    $uid=API::INP('uid');    
+    $fid=API::INP('fid');
+    //要确保fid是对应一个存在的数据
+    $r=FEED::feed_get($uid,$fid,'publish');
+    if(API::is_error($r)){
+      return $r;
+    }
+    $r=FEED::feed_delete( $fid );
+    return API::data($r);
+  }  
+  
+}
+
---
+++ src/system/include/class.FEED.php
@@ -0,0 +1,252 @@
+<?php
+
+class FEED { 
+  
+  //获取 数据表名
+  static function table_name( $item='feed' ) {
+    $prefix=api_g("api-table-prefix");
+    return $prefix.$item;
+  }
+  
+  static function data_val( $key, & $data ) {
+    if(false === API::INP($key)) return;
+    $data[$key]=API::INP($key);
+  }
+  
+  //TODO: 有效性检查
+  static function data_all( ) {
+    $data=[];
+    self::data_val('app',$data);
+    self::data_val('cat',$data);
+    self::data_val('k1',$data);
+    self::data_val('k2',$data);
+    self::data_val('k3',$data);
+    self::data_val('k4',$data);
+    self::data_val('content',$data);
+    self::data_val('pics',$data);
+    self::data_val('d1',$data);
+    self::data_val('d2',$data);
+    self::data_val('d3',$data);
+    self::data_val('d4',$data);
+    self::data_val('attr',$data);
+    
+    $data['update_at']=time();
+    return $data;
+  }
+  
+  //-----------------------------------------------
+  
+  // C--- 新建  【草稿】
+  static function draft_create( $uid ) {
+    $db=api_g('db');
+    $tblname=self::table_name();
+    $data=[
+      'uid'=>$uid,
+      'flag'=>'draft',
+      'del'=>0,
+      'app'=>'',
+      'cat'=>'',
+      'k1'=>'',
+      'k2'=>'',
+      'k3'=>'',
+      'k4'=>'',
+      'content'=>'',
+      'pics'=>'',
+      'update_at'=>time(),
+      'd1'=>'',
+      'd2'=>'',
+      'd3'=>'',
+      'd4'=>'',
+      'attr'=>''
+    ];
+    $r=$db->insert($tblname,$data );
+    if(!$r)return false;
+    $data['fid']=$r;
+    return $data;
+  }
+
+  // -R-- 获取 uid 的最新【草稿】
+  static function draft_get_by_uid( $uid ) {
+    $db=api_g('db');
+    $tblname=self::table_name();
+    $r=$db->get($tblname,
+      self::feed_columns(),
+      ['and'=>['uid'=>$uid,'flag'=>'draft','del'=>0]]);
+    
+    return $r;
+  }
+  
+  // -R-- 获取 uid 的最新【已删除草稿】
+  static function draft_get_deleted_by_uid( $uid ) {
+    $db=api_g('db');
+    $tblname=self::table_name();
+    $r=$db->get($tblname,
+      self::feed_columns(),
+      ['and'=>['uid'=>$uid,'flag'=>'draft','del'=>1]]);
+    
+    return $r;
+  }
+  
+  // --U- 更新 草稿 同  feed
+
+  // ---D 删除 草稿 同  feed
+
+  //发布 【草稿】
+  static function draft_publish( $fid ) {
+    $db=api_g('db');
+    $tblname=self::table_name();
+
+    //发布的算法：
+    //1,复制一份草稿为 正式 publish
+    //2,然后把草稿文字内容清空（科目等保留）
+    $sth = $db->pdo->prepare("INSERT INTO $tblname 
+      (`app`,`cat`,`k1`,`k2`,`k3`,`k4`,`content`,`pics`,
+        `d1`,`d2`,`d3`,`d4`,`attr`,
+        `uid`,`flag`,`del`,`publish_at`)
+      SELECT `app`,`cat`,`k1`,`k2`,`k3`,`k4`,`content`,`pics`,
+        `d1`,`d2`,`d3`,`d4`,`attr`,
+        :uid ,'publish','0', :now
+      FROM $tblname 
+      WHERE fid = $fid" );
+ 
+    $uid=API::INP('uid');
+    $now=time();
+    $sth->bindParam(':uid', $uid, PDO::PARAM_INT);
+    $sth->bindParam(':now', $now, PDO::PARAM_INT);
+     
+    $sth->execute();
+    
+    return self::feed_update($fid,['content'=>'','pics'=>'']);
+  }
+  
+  
+  //WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW
+  static function feed_columns() {
+    return
+      ['fid','uid','flag','del',
+      'app','cat','k1','k2','k3','k4',
+      'content','pics',
+      'd1','d2','d3','d4','attr',
+      'create_at','update_at'];
+  }
+  // C--- 不提供创建 feed 的接口，使用草稿的发布功能创建 feed
+  
+  // -R-- 获取
+  static function feed_get( $uid, $fid, $type='publish',$include_del=false ) {
+    if(!$fid) {
+      return API::msg(202101,"fid required");
+    }
+    $db=api_g('db');
+    $tblname=self::table_name();
+    $r=$db->get($tblname,
+      self::feed_columns(),
+      ['and'=>['fid'=>$fid]]);
+      
+    if(!$r) {
+      return API::msg(202102,"fid $fid not exist");
+    }
+    
+    //草稿只允许自己看
+    if($type=='draft' && $r['uid']!=$uid) {
+      return API::msg(202103,"draft $fid is not belongs to uid $uid");
+    }
+    if($r['flag']!=$type) {
+      return API::msg(202104,"fid $fid is not $type");
+    }
+    if( !$include_del && $r['del']) {
+      return API::msg(202105,"fid $fid was deleted yet");
+    }
+    return API::data($r);
+  }
+  
+  // -R-- 获取
+  static function feed_list( $uid, $type='publish',$include_del=false ) {
+    $db=api_g('db');
+    $tblname=self::table_name();
+    
+    $andArray=[];
+    $tik=0;
+    
+    $oldmore=API::INP('oldmore');
+    if($oldmore) {
+      $tik++;
+      $andArray["and#t$tik"]=['fid[<]'=>intval($oldmore)];
+    }
+    $newmore=API::INP('newmore');
+    if($newmore) {
+      $tik++;
+      $andArray["and#t$tik"]=['fid[>]'=>intval($newmore)];
+    }
+    
+    
+    
+    $tik++;
+    $and=['flag'=>$type];
+    $andArray["and#t$tik"]=$and;
+    
+
+    $and_DEL=false;
+    if($include_del=='only') {
+      $and_DEL=['del'=>1];
+    } else if( ! $include_del) {
+      $and_DEL=['del'=>0];
+    }
+    if($and_DEL) {
+      $tik++;
+      $andArray["and#t$tik"]=$and_DEL;
+    }
+    
+    
+    $count=intval(API::INP('count'));
+    if($count==0)$count=20;
+    else if($count<2)$count=20;
+    else if($count>200)$count=200;
+    
+    $where=["LIMIT" => $count , "ORDER" => ["publish_at DESC", "update_at DESC"]] ;
+    if(count($andArray))
+      $where['and'] = $andArray ;
+
+
+    $r=$db->select($tblname,self::feed_columns(),$where);
+      
+      
+    //var_dump($db);
+    if(!$r) {
+      return API::msg(202003,"nothing");
+    }
+    
+    return API::data($r);
+  }
+
+  // --U- 更新
+  static function feed_update( $fid, $data ) {
+    $db=api_g('db');
+    $tblname=self::table_name();
+    $r=$db->update($tblname, $data,
+      ['and'=>['fid'=>$fid],'LIMIT'=>1]);
+    return $r;
+  }
+  // ---D 删除
+  static function feed_delete( $fid ) {
+    return self::feed_update($fid,['del'=>1]);
+  }
+  
+  //QQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQ
+
+  //判断是否有效，主要用于草稿发布为正式feed  
+  static function feed_validate($feed) {
+    $err='';
+    if( !$feed['content'] && !$feed['pics']) {
+      $err.='发布内容是空的。';
+    }
+    return $err;
+  }
+  // 撤销删除
+  static function feed_undelete( $fid ) {
+    return self::feed_update($fid,['del'=>0]);
+  }
+  // 撤销为草稿
+  static function feed_to_draft( $fid ) {
+    return self::feed_update($fid,['flag'=>'draft']);
+  }
+}
