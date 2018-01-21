42b99777da311a9001da2b3dfba1fe47ff089813
linjianhong <linjianhong@github.com>
2018-01-21 21:29:14 +0800

    添加：api-shell

---
+++ src/dj-api-shell/api-db.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * PHP 接口数据库层
+ *
+ */
+
+namespace DJApi;
+
+require_once( "lib/medoo/medoo.php");
+require_once( "api-root.php");
+use DJApi\API;
+use DJApi\Configs;
+
+class DB extends \medoo {
+  public static $_db;
+  public static function db(){
+    if(!self::$_db) self::$_db = new DB();
+    return self::$_db;
+  }
+  /**
+   * 构造函数，保存参数
+   */
+  public function __construct($options = []) {
+    $defaults = Configs::get(['main_db', 'medoo']);
+    $keys = [
+      'database_type',
+      'database_name',
+      'server'       ,
+      'port'         ,
+      'username'     ,
+      'password'     ,
+      'charset'
+    ];
+    foreach($keys as $k){
+      $optn[$k] = $options[$k]? $options[$k]: $defaults[$k];
+    }
+    //echo "mymedoo:";var_dump($optn);
+    parent::__construct($optn);
+  }
+  static function table($name){
+    $tableName = Configs.get(["main_db", "tableName", $name]);
+    return $tableName ? $tableName : $name;
+  }
+  //DEBUG
+  public function show() {
+    echo "database_name = " .($this->database_name)."<br>";
+    var_dump($this->last_query());
+    var_dump($this->error());
+  }
+  public function getShow() {
+    return [
+      "DB"         => $this->database_name,
+      "last_query" => $this->last_query(),
+      "error"      => $this->error()
+    ];
+  }
+}
+
---
+++ src/dj-api-shell/api-request.php
@@ -0,0 +1,82 @@
+<?php
+/**
+ * PHP 接口请求层
+ *
+ * ------- file: index.php --------
+  require_once( "api-request.php");
+  use DJApi\Request\Request;
+  use DJApi\API;
+
+  $api   = isset($_GET['api'    ])?trim($_GET['api'    ]):'';//TODO: 这里需要验证合法文件名
+  $call  = isset($_GET['call'   ])?trim($_GET['call'   ]):'';//TODO: 这里需要验证合法函数名
+  $para1 = isset($_GET['__para1'])?trim($_GET['__para1']):'';
+  $para2 = isset($_GET['__para2'])?trim($_GET['__para2']):'';
+
+  $options = [
+    "para1" => $para1,
+    "para2" => $para2
+  ];
+  $request = new Request($api, $call, $options);
+
+  echo API::cn_json($request->getJson());
+
+ */
+
+namespace DJApi;
+
+require_once( "api-root.php");
+use DJApi\API;
+
+
+class Request {
+  /**
+   * 构造函数，保存参数
+   */
+  function __construct($api, $call, $options = []) {
+    $this->api = $api;
+    $this->call  = $call;
+    $this->para1 = $options['para1'];
+    $this->para1 = $options['para1'];
+    if(!$this->call ) $this->call  = 'main';
+    if(!$this->para1) $this->para1 = '';
+    if(!$this->para1) $this->para1 = '';
+  }
+
+  /**
+   * 执行 api 调用，并返回 json 数据
+   */
+  function getJson(){
+    $apiFile = $this->getApiFile();
+    if(!$apiFile){
+      return API::error(API::E_API_NOT_EXITS, '请求错误');
+    }
+
+    require_once($apiFile);
+    $C="class_{$this->api}";
+    if(! class_exists($C) ) {
+      return API::error(API::E_CLASS_NOT_EXITS, '请求错误', [$this, $apiFile, $C]);
+    }
+
+    if( method_exists($C, 'API') ) {
+      // 调用共享 API 函数
+      return $C::API($this->call, $para1, $para2);
+    }
+    else if(! method_exists($C,$this->call) ) {
+      return API::error(API::E_FUNCTION_NOT_EXITS, '请求错误');
+    }
+    return $C::$this->call($para1, $para2);
+  }
+
+  /**
+   * （内部函数）
+   * 根据构造的参数，获取需要调用的文件名
+   */
+  protected function getApiFile(){
+    $apiFile = dirname($_SERVER['SCRIPT_FILENAME']) . "/apis/api_{$this->api}.php";
+    if(file_exists($apiFile)){
+      return $apiFile;
+    }
+    return false;
+  }
+}
+
