98bd92afd0161263f4dc9670e0ffbc8832be3d39
linjianhong <linjianhong@github.com>
2018-01-23 11:50:14 +0800

    使用记录模块 1. 可获取使用记录

---
+++ src/use-records/数据库.sql
@@ -0,0 +1,16 @@
+--
+-- 表的结构 `api_tbl_use_record`
+--
+
+CREATE TABLE `api_tbl_use_record` (
+  `id`   int(11) not null auto_increment,
+  `uid`  int(11) NOT NULL COMMENT '用户id',
+  `time` varchar( 32) DEFAULT '' COMMENT  '时间',
+  `k1`   varchar( 32) DEFAULT '' COMMENT  '主分类',
+  `k2`   varchar( 32) DEFAULT '' COMMENT  '次分类',
+  `v1`   varchar(128) DEFAULT '' COMMENT  '主值',
+  `v2`   varchar(128) DEFAULT '' COMMENT  '次值',
+  `n`    varchar( 11) DEFAULT '' COMMENT  '数量',
+  primary key (`id`),
+  unique key `id` (`id`)
+) DEFAULT CHARSET=utf8;
