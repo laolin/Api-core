f57756b86c158e54a351e4027d2d04fe1ec66b99
linjianhong <linjianhong@github.com>
2018-01-25 19:56:06 +0800

    使用记录模块：接口 data/record, 将使用情况予以记录

--- src/local-api/use-records/apis/api_data.php
+++ src/local-api/use-records/apis/api_data.php
@@ -9,6 +9,37 @@ use DJApi;
 
 class class_data{
   static $tableName = 'api_tbl_use_record';
+
+
+  /**
+   * 将使用情况予以记录
+   * api地址: data/record
+   *
+   * @query module 模块名称
+   * @query uid 用户id
+   * @query k1 主分类
+   * @query k2 次分类(可选)
+   * @query v1 主值(可选)
+   * @query v2 次值(可选)
+   * @query n 数量
+   */
+  public static function record($request) {
+    $db = DJApi\DB::db();
+
+    $id = $db->insert(self::$tableName, $aa = [
+      'time'   => DJApi\API::now(),
+      'module' => $request->safeQuery('module', '无模块'),
+      'uid'    => $request->safeQuery('uid'   , 0),
+      'k1'     => $request->safeQuery('k1'    ),
+      'k2'     => $request->safeQuery('k2'    ),
+      'v1'     => $request->safeQuery('v1'    ),
+      'v2'     => $request->safeQuery('v2'    ),
+      'n'      => $request->safeQuery('n'     , 0)
+    ]);
+
+    return DJApi\API::OK(['n' => $id? 1: 0, 'aa'=>$aa, 'query'=>$request->query]);
+  }
+
   public static function count($request) {
 
     $fieldsAll = ['uid', 'time', 'k1', 'k2'];
