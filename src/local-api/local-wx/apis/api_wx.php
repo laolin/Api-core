7d85f7a6691cf9aad55ee174ee450375ec44c909
linjianhong <linjianhong@github.com>
2018-01-25 09:31:59 +0800

    添加：本地独立api：微信接口

---
+++ src/local-api/local-wx/apis/api_wx.php
@@ -0,0 +1,21 @@
+<?php
+namespace DJApi\LocalWx;
+
+use DJApi\API;
+use DJApi\Configs;
+use DJApi\DB;
+
+class class_wx{
+
+  /**
+   * 接口： wx/access_token
+   * 获取 access_token
+   * @request name: 公众号名称
+   */
+  public static function access_token($request){
+    $name = $request->query['name'];
+    if(!$name)return API::error(1001, "参数无效");
+    return DjApi::post(SERVER_API_ROOT, "server-wx/wx/access_token", ['name'=>$name]);
+  }
+
+}
