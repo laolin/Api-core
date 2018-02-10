cd72813773bf33cb7635c4f6e764a5c993f6b6b0
linjianhong <linjianhong@github.com>
2018-02-10 09:01:32 +0800

    添加：微信类库

---
+++ src/apis/classes/MyClass/WX.php
@@ -0,0 +1,24 @@
+<?php
+// ================================
+/*
+*/
+namespace MyClass;
+use DJApi;
+
+class WX extends SteeStatic{
+
+  /** 获取微信信息
+   * @param userid: 用户id，可为数组或单个用户id
+   * 返回：
+   * @return 微信信息数组
+   */
+  static function getWxInfo($userid) {
+    return DJApi\DB::db()->select(self::$table['user'], ["[>]" .self::$table['wx']=>['uid' => 'uidBinded']],
+     ['headimgurl', 'uidBinded', 'nickname', self::$table['wx'].'.id'],
+     ["AND" =>[
+       'appFrom' => DJApi\Configs::get(["WX_APPID_APPSEC_DEFAULT", "WX_APPID"]),
+       "uidBinded" => $userid,
+      ]]
+    );
+  }
+}
