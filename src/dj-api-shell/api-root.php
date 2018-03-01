f8e91ca08e3c1329e74add813c6360037b37f552
linjianhong <linjianhong@github.com>
2018-03-01 19:40:19 +0800

    api-shell: 添加 is_https 函数

--- src/dj-api-shell/api-root.php
+++ src/dj-api-shell/api-root.php
@@ -193,6 +193,16 @@ class API{
   // ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
   // ┃             后台网页请求                                               ┃
   // ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
+  static function is_https() {
+    if ( !empty($_SERVER['HTTPS']) && strtolower($_SERVER['HTTPS']) !== 'off') {
+      return true;
+    } elseif ( isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https' ) {
+      return true;
+    } elseif ( !empty($_SERVER['HTTP_FRONT_END_HTTPS']) && strtolower($_SERVER['HTTP_FRONT_END_HTTPS']) !== 'off') {
+      return true;
+    }
+    return false;
+  }
   static function httpGet($url) {
     $curl = curl_init();
     curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
