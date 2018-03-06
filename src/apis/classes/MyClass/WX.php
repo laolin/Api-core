7b41791d6d3a6c6f88e91cd55e3076081e909da7
linjianhong <linjianhong@github.com>
2018-03-06 17:42:58 +0800

    获取微信信息：使用独立api调用

--- src/apis/classes/MyClass/WX.php
+++ src/apis/classes/MyClass/WX.php
@@ -8,21 +8,28 @@ use DJApi;
 class WX extends SteeStatic{
 
   /** 获取微信信息
-   * @param userid: 用户id，可为数组或单个用户id
+   * @param uid: 用户id，可为数组或单个用户id
    * 返回：
    * @return 微信信息数组
    */
-  static function getWxInfo($userid) {
-    return DJApi\DB::db()->select(
-      self::$table['user'], ["[>]" .self::$table['wx']=>['uid' => 'uidBinded']],
-      ['headimgurl', 'uidBinded', 'nickname', self::$table['wx'].'.id'],
-      [
-        "AND" =>[
-          'appFrom' => DJApi\Configs::get(["WX_APPID_APPSEC_DEFAULT", "WX_APPID"]),
-          "uidBinded" => $userid,
-        ],
-        "LIMIT" => 1000
-      ]
-    );
+  static function getWxInfo($uid) {
+
+    $wxInfoJson = \DJApi\API::post(SERVER_API_ROOT, "user/mix/wx_infos", ['uid' => $uid]);
+    \DJApi\API::debug(['从独立服务器获取微信信息', $wxInfoJson]);
+    if(!\DJApi\API::isOk($wxInfoJson)){
+      return $wxInfoJson;
+    }
+    $wxInfo = $wxInfoJson['datas']['list'];
+
+    // 两者合并
+    $list = [];
+    foreach($wxInfo as $k=>$row){
+      $list[] = [
+        'headimgurl'=>$row['headimgurl'],
+        'nickname'  =>$row['nickname'  ],
+        'uidBinded' =>$row['uid']
+      ];
+    }
+    return $list;
   }
 }
