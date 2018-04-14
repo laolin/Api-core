0fb6c035ae6a28c181f169cc7ce4fd389a5ac5e5
linjianhong <linjianhong@github.com>
2018-04-14 14:42:15 +0800

    添加接口 app/getWxInfo: 用户id获取微信信息

--- src/cmoss/apis/apis-user/api_app.php
+++ src/cmoss/apis/apis-user/api_app.php
@@ -103,5 +103,35 @@ class class_app {
 
     return \DJApi\API::OK($data);
   }
+  /** 
+   * 接口： app/getWxInfo
+   * 获取微信信息
+   * @param uid: 用户id，可为数组或单个用户id
+   * 返回：
+   * @return 微信信息数组
+   */
+  static function getWxInfo($request) {
+    $uid = $request->query['uid'];
+    \DJApi\API::debug(['参数 uid = ', $uid]);
+
+    $wxInfoJson = \DJApi\API::post(SERVER_API_ROOT, "user/mix/wx_infos", ['uid' => $uid]);
+    \DJApi\API::debug(['从独立服务器获取微信信息', $wxInfoJson]);
+    if(!\DJApi\API::isOk($wxInfoJson)){
+      return $wxInfoJson;
+    }
+    $wxInfo = $wxInfoJson['datas']['list'];
+
+    // 整理
+    $list = [];
+    foreach($wxInfo as $k=>$row){
+      $list[] = [
+        'headimgurl'=>$row['headimgurl'],
+        'nickname'  =>$row['nickname'  ],
+        'uid'       =>$row['uid']
+      ];
+    }
+    return \DJApi\API::OK(['list'=>$list]);
+  }
+
 
 }
