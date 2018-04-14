c22dae28001dc7fae1ff21a0632225c0fbf92cd7
linjianhong <linjianhong@github.com>
2018-04-14 14:45:19 +0800

    推送产能给项目、推送项目给产能: 权限限制

--- src/cmoss/apis/apis-user/api_stee_msg.php
+++ src/cmoss/apis/apis-user/api_stee_msg.php
@@ -127,13 +127,8 @@ class class_stee_msg{
       ]
     ])['datas']['R'];
     $rightName = $from_type == 'steefac' ? '推送产能给项目' : '推送项目给产能';
-    if(!$uid || (!in_array($uid, $uidGroup['from'])) && \MyClass\CRoleright::hasRight($uid, $rightName)){
-      return DJApi\API::error(DJApi\API::E_NEED_RIGHT, '不是管理员', ['uid'=>$uid, 'uidGroup'=>$uidGroup, 'query'=>[
-        'from_type' => $from_type,
-        'from_id'   => $from_id,
-        'to_type'   => $to_type,
-        'to_ids'    => $to_ids
-      ]]);
+    if(!$uid || (!in_array($uid, $uidGroup['from'])) && !\MyClass\CRoleright::hasRight($uid, $rightName)){
+      return DJApi\API::error(DJApi\API::E_NEED_RIGHT, '不是管理员', ['uid'=>$uid]);
     }
 
     // 获取 openid 列表
